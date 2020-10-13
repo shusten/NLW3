03e4170421e78542c84e3090d9e69d0fcc1a1c2b
Luis Cov Jr <shusten@hotmail.com>
2020-10-13 02:38:14 -0300

    :construction: chore: adiciona estrotura da pagina de orfaos

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+function OrphanagesMap() {
+    return ( 
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src="" alt=""/>
+                </header>
+            </aside>
+        </div>
+        )
+}
+
+export default OrphanagesMap;

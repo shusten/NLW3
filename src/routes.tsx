542ae3cf8199eb64f53e220b679baca6a5cce0d7
Luis Cov Jr <shusten@hotmail.com>
2020-10-13 02:09:58 -0300

    :recycle: refactor: refatorando usando rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Landing from './pages/Landing';
+
 function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" component={} />
+            <Route path="/" component={Landing} />
         </BrowserRouter>
     )
 }
